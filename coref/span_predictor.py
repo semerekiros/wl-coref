d6b131e9d923512608b014855d0d7e2795700e6b
Karel D'Oosterlinck <kareldoosterlinck@gmail.com>
2023-02-05 16:44:27 +0100

    resolve error with maxpool

--- coref/span_predictor.py
+++ coref/span_predictor.py
@@ -129,7 +129,7 @@ class SpanPredictor(torch.nn.Module):
         for cluster in clusters:
             if self.cluster_averaging == 'mean':
                 cluster_averages.append(torch.mean(words[cluster], 0))
-            elif self.cluster_averaging == 'max':
+            elif self.cluster_averaging == 'maxpool':
                 cluster_averages.append(torch.max(words[cluster], 0))
 
         cluster_averages = torch.stack(cluster_averages)
